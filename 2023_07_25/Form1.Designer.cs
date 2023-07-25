44dce82538b549f6c610b98c65729418c6e35eca
С - 3 < - 3@DESKTOP-E0FTS21>
2023-07-25 21:28:29 +0300

    Добавьте файлы проекта.

---
+++ 2023_07_25/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace _2023_07_25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 61);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(274, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 61);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(59, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 61);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(443, 83);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 61);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "button1";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(482, 254);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 61);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "button1";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(289, 172);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(138, 61);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "button1";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(751, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 124);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 577);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 2023_07_25/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2023_07_25
+{   
+    public partial class Form1 : Form
+    {
+        Point start;
+        Point finish;
+        Graphics g;
+        Rectangle rectangle;
+        List<Control> controlsLst = new List<Control>();
+
+        bool MouseDown = false;
+        public Form1()
+        {
+            InitializeComponent();
+            /*var tmp = Controls.OfType<Button>().ToArray();
+            string name = string.Empty;
+            foreach(var item in tmp)
+            {
+                MessageBox.Show(((Control)item).Name);
+                name += ((Control)item).Name + "\n";
+            }
+            MessageBox.Show(name);*/
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = e.Location;
+            MouseDown = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            finish = e.Location;
+            var size = new Size(Math.Abs((start.X - finish.X)),
+                Math.Abs(start.Y - finish.Y));
+            Point _start = new Point(start.X < finish.X ? start.X:finish.X, 
+                start.Y < finish.Y? start.Y:finish.Y);
+            Rectangle _rectangle = new Rectangle(start, size);
+            this.rectangle = _rectangle;
+            g = ((Form)sender).CreateGraphics();
+            Brush brush = new SolidBrush(Color.Blue);
+            //g.FillRectangle(brush, _rectangle);
+            Pen pen = new Pen(Color.Blue, 2);
+            g.DrawRectangle(pen, rectangle);
+            listBox1.Items.Clear();
+            string name = string.Empty;
+            foreach(var item in controlsLst)
+            {
+                if(item is Button)
+                {
+                    listBox1.Items.Add(((Button)item).Text);                   
+                }
+            }
+            //MessageBox.Show(name);
+        }
+        private void addButton(Point _location)
+        {
+            Button button = new Button();
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+             
+        }
+    }
+}
